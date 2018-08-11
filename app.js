7667c538c526ed23f16ea0bd838dcc263609719b
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2018-08-11 16:00:56 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,96 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Burgerbotz ready! :3');
+});
+
+function randomize(min, max) 
+{
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+const prefix = "/";
+
+client.on('message', message => 
+{
+    function post(String)
+		{
+			message.channel.send(String);
+			return 0;
+		}
+		
+		if(message.author === client.user) return;
+		
+		let user = message.mentions.users.first();
+		let target = message.guild;
+		
+		
+		var msg = message.content.toLowerCase();
+		const args = msg.slice(prefix.length).trim().split(/ +/g);
+
+		if(message.content.startsWith("+s"))
+		{
+			args[0] = "";
+			var x = randomize(0, 5);
+			if(x > 1)
+			{
+				post(`${message.author}` + " has spitted on" + args.join(" ") + "'s grave :purple_heart:");
+			} else {
+				post(`${message.author}` + " has took a shit on" + args.join(" ") + "'s grave :poo:");
+			}
+		}
+		
+		const command = args.shift().toLowerCase();
+		
+		switch(command)
+		{
+				case "ping":
+					post("***PONG!*** Returned request in " + (Date.now() - message.createdTimestamp) + " ms");
+					break;
+					
+				case "random":
+					var x = parseInt(args[0]);
+					post("Returned integer " + Math.floor(Math.random() * x));
+					break;
+					
+				case "coinflip":
+					var x = Math.floor(Math.random() * 2);
+					if(x === 0)
+					{
+						message.reply("you got heads");
+					} else {
+						message.reply("you got tails");
+					}
+					break;
+					
+				case "rape":
+					if(message.mentions.users.size < 1)
+					{
+						message.reply("please specify a user to *rape*!");
+					} else {
+					message.channel.send(`${user} has been brutally sodomized by ${message.author}`, {files: ["https://img.4plebs.org/boards/sp/image/1405/27/1405279865972.gif"]});
+					}
+					break;
+					
+				case "dm":
+				{
+					if(message.mentions.users.size < 1)
+					{
+						message.reply("please specify a user to message.");
+					} else {
+						delete args[0];
+						user.send(args.join(" "));
+					}
+					break;
+				}
+			
+				case "post":
+					post(args.join(" "));
+					break;
+
+		}
+});
+
+client.login(process.env.BOT_TOKEN);
