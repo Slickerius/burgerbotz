82160b434eae5e317e2e552f0e1ff75703411de7
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2018-08-11 16:49:32 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on('ready', () => 
+{
     console.log('Burgerbotz ready! :3');
 });
 
