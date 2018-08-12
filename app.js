0173b2994cbb102b55ac484ac559fc7659104d1c
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2018-08-12 14:24:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
+
+const database = JSON.parse(fs.readFileSync('data/userData.json', 'utf8'));
 
 client.on('ready', () => 
 {
