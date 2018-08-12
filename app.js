239f6926672725e17cb428a2b112cefd2f54f495
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2018-08-12 18:44:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -143,7 +143,7 @@ client.on('message', message =>
 							} else {
 								database[player2ID].hp -= damage;
 								database[player1ID].ammo -= 1;
-								post(player1Name + " has shot " + player2Name + ", dealing " + healPoints + " HP");
+								post(player1Name + " has shot " + player2Name + ", dealing " + damage + " HP");
 								
 								turnID = player2ID;
 								tabScreen(player2Name, player1ID, player2ID, player1Name, player2Name);	
@@ -168,7 +168,7 @@ client.on('message', message =>
 							} else {
 								database[player1ID].hp -= damage;
 								database[player2ID].ammo -= 1;
-								post(player2Name + " has shot " + player1Name + ", dealing " + healPoints + " HP");
+								post(player2Name + " has shot " + player1Name + ", dealing " + damage + " HP");
 								
 								turnID = player1ID;
 								tabScreen(player1Name, player1ID, player2ID, player1Name, player2Name);	
