eb977399ceb9a54e0a7461c94401e91687938894
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2018-08-13 20:17:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -405,7 +405,19 @@ client.on('message', message =>
 					{
 						database[user.id] = {nigger: result};
 					}
-					post("<:dindu:454150474619289602> ***__Niggerator X3000__*** <:dindu:454150474619289602>\n\n:bar_chart: ***" + user.username + "*** : ***" + database[user.id].nigger + "%***");
+					
+					var n = database[user.id].nigger;
+					
+					if(n >= 0 && n < 30)
+					{
+						post("<:dindu:454150474619289602> ***__Niggerator X3000__*** <:dindu:454150474619289602>\n\n:bar_chart: **" + user.username + "** : **" + database[user.id].nigger + "%**\n*Green flag! This person shows little to no signs of being a nigger.*");
+					} else if(n >= 30 && n < 50) {
+						post("<:dindu:454150474619289602> ***__Niggerator X3000__*** <:dindu:454150474619289602>\n\n:bar_chart: **" + user.username + "** : **" + database[user.id].nigger + "%**\n*This person could be a nigger for all we know, even though it's unlikely. Be safe!*");	
+					} else if(n >= 50 && n < 75) {
+						post("<:dindu:454150474619289602> ***__Niggerator X3000__*** <:dindu:454150474619289602>\n\n:bar_chart: **" + user.username + "** : **" + database[user.id].nigger + "%**\n*Be careful! This person is very likely to be a nigger! Utter not the forbidden word for your own sake!*");	
+					} else {
+						post("<:dindu:454150474619289602> ***__Niggerator X3000__*** <:dindu:454150474619289602>\n\n:bar_chart: **" + user.username + "** : **" + database[user.id].nigger + "%**\n*Red flag. This person is almost undoubtedly a nigger. Avoid it at all costs.*");	
+					}
 				}
 				break;
 			
