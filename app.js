e99a85a08e442cf3f742bab311d5d7e522b8b0a6
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2018-08-14 20:39:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -497,7 +497,7 @@ client.on('message', message =>
 				.setThumbnail(client.user.avatarURL)
 				.setColor("#fcc66a")
 				.setTitle(":hamburger: Burgerbotz :hamburger:")
-				.setDescription("Invite me to your server! Burgerbotz is a first-time experience bot designed and created by ***Arbaz Williamson#4357***. I personally would like to thank *Naga*, *Slick*, and *Annabelle* for their contributions to this project.");
+				.setDescription("Invite me to your server! Burgerbotz is a first-time experience bot designed and created by ***Arbaz Williamson#4357***. I personally would like to thank *Naga*, *Slick*, and *Annabelle* for their contributions to this project.")
 				.setFooter("https://discordapp.com/oauth2/authorize/?permissions=8&scope=bot&client_id=477763761629954060");
 				
 				message.channel.send(botembed);
