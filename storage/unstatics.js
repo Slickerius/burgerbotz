bc47c6a800616daf830f297effd73abe6fdf3b21
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2018-08-15 17:25:50 +0700

    Update unstatics.js

--- storage/unstatics.js
+++ storage/unstatics.js
@@ -4,7 +4,10 @@ burger = [
   "https://gif-free.com/uploads/posts/2018-01/1517419327_anime-girl-eating-burger.gif", 
   "https://i.gifer.com/nwL.gif", 
   "https://i.gifer.com/ElSx.gif", 
-  "https://img.fireden.net/a/image/1486/57/1486574417193.gif"];
+  "https://img.fireden.net/a/image/1486/57/1486574417193.gif",
+  "https://cdn.discordapp.com/attachments/458082060607684608/478966167906549763/image.gif",
+  "https://i.warosu.org/data/jp/img/0124/38/1409634459946.gif",
+  "https://i.gifer.com/5MQq.gif"];
 
 rape = [
   "https://img.4plebs.org/boards/sp/image/1405/27/1405279865972.gif", 
