261a895440a17de934ed27eefe8a351287585758
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2018-08-15 16:44:06 +0700

    Update app.js

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ const prefix = '/';
 const helpTab = "```/afk - Sets away from keyboard status\n/battle - Challenges another user to a battle!\n/burger - A burger a day keeps the doctor away!\n/coinflip - Flips a coin\n/dm - Sends DM to a user\n/help - Shows this help screen\n/invite - Invite me to your server!\n/nigger - Calculates a user's niggerness\n/ping - Pong\n/post - Posts a message\n/random - Generates a random number\n/rape - Rapes a user\n/s - Spits on grave```";
 
 const invite = "\uD83C\uDF80\n<https://bit.ly/2KQn8fX>";
-const rapeGifs = ["https://img.4plebs.org/boards/sp/image/1405/27/1405279865972.gif", "https://media1.tenor.com/images/dc7284a153bbc22dd2ddda022957e2be/tenor.gif", "https://i.pinimg.com/originals/ce/41/9c/ce419c7a3723967df36b4a3c7ddfc8ac.gif", "https://i.pinimg.com/originals/9c/11/f6/9c11f69bdf4517a6226f57875b163c20.gif", "https://media.giphy.com/media/TxBXGd0h4kVS8/giphy.gif"];
-const burgerGifs = ["https://i.gifer.com/Qlsi.gif", "https://media.giphy.com/media/EmSCxtcjQCmXK/giphy.gif", "https://gif-free.com/uploads/posts/2018-01/1517419327_anime-girl-eating-burger.gif", "https://i.gifer.com/nwL.gif", "https://i.gifer.com/ElSx.gif", "https://img.fireden.net/a/image/1486/57/1486574417193.gif"];
+const rapeGifs = require('RapeGIFs.js');
+const burgerGifs = require('BurgerGIFs');
 
 var inGame = false;
 var inRequest = false;
