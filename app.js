148bb6c72f9dfd734cbe2cf1fd3bb7ebb36c17e2
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2018-08-17 16:34:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -388,7 +388,7 @@ client.on('message', message =>
 		var msg = message.content.toLowerCase();
 		const args = msg.slice(prefix.length).trim().split(/ +/g);
 	
-		if(database[sender.id].afk && cmd != "afk")
+		if(database[sender.id].afk && cmd != prefix + "afk")
 		{
 			database[sender.id].afk = false;
 			database[sender.id].afkMessage = "";
