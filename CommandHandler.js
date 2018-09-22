53a52256bcc1502bcec7ec0680acbe5d3207afd9
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2018-09-22 15:15:06 +0700

    Update CommandHandler.js

--- CommandHandler.js
+++ CommandHandler.js
@@ -1,12 +1,36 @@
+const Discord = require('discord.js');
+
+function randomize(min, max) 
+{
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
 module.exports = 
 {
-    post: function(channel, arg) 
-    {
-        if(arg.length > 1)
-				{
-					return channel.send(arg);
-				} else {
-					return channel.send("Correct usage: /post <message>");
-				}
-    }
+	post: function(channel, arg) 
+	{
+        	if(arg.length > 1)
+		{
+			return channel.send(arg);
+		} else {
+			return channel.send("Correct usage: /post <message>");
+		}
+	},
+	
+	rape: function(channel, message)
+	{
+		if(message.mentions.users.size < 1)
+		{
+			return message.reply("please specify a user to *rape*!");
+		} else {
+			let x = randomize(0, rapeGifs.length);
+
+			let botembed = new Discord.RichEmbed()
+			.setImage(rapeGifs[x])
+			.setDescription(`**${user.username}** has been brutally sodomized by **${sender.username}**`)
+			.setColor("#fcc66a");
+				
+			return message.channel.send(botembed);
+		}
+	}
 };
