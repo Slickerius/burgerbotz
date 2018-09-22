568d13147e49fbb05abf84278148dc0aad64618b
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2018-09-22 15:25:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -502,31 +502,7 @@ client.on('message', message =>
 				break;
 				
 			case "nigger":
-				if(message.mentions.users.size < 1)
-				{
-					message.reply("you have to mention someone to calculate the niggerness of!");
-				} else {
-					var result = randomize(0,100);
-					if(!database[user.id]) database[user.id] = {nigger: result};
-					
-					if(database[user.id].nigger == null)
-					{
-						database[user.id] = {nigger: result};
-					}
-					
-					var n = database[user.id].nigger;
-					
-					if(n >= 0 && n < 30)
-					{
-						post("<:dindu:454150474619289602> ***__Niggerator X3000__*** <:dindu:454150474619289602>\n\n:bar_chart: **" + user.username + "** : **" + database[user.id].nigger + "%**\n*Green flag! This person shows little to no signs of being a nigger.*");
-					} else if(n >= 30 && n < 50) {
-						post("<:dindu:454150474619289602> ***__Niggerator X3000__*** <:dindu:454150474619289602>\n\n:bar_chart: **" + user.username + "** : **" + database[user.id].nigger + "%**\n*This person could be a nigger for all we know, even though it's unlikely. Be safe!*");	
-					} else if(n >= 50 && n < 75) {
-						post("<:dindu:454150474619289602> ***__Niggerator X3000__*** <:dindu:454150474619289602>\n\n:bar_chart: **" + user.username + "** : **" + database[user.id].nigger + "%**\n*Be careful! This person is very likely to be a nigger! Utter not the forbidden word for your own sake!*");	
-					} else {
-						post("<:dindu:454150474619289602> ***__Niggerator X3000__*** <:dindu:454150474619289602>\n\n:bar_chart: **" + user.username + "** : **" + database[user.id].nigger + "%**\n*This person is almost undoubtedly a nigger.* :watermelon:**OOGA BOOGA DINDU NUFFIN!!1**:basketball:");	
-					}
-				}
+				handler.nigger(ch, message);
 				break;
 			
 			case "post":
