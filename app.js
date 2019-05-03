60573ac324c4ce812fa32ffb85ab9ebcff813d56
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2019-05-03 15:20:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -570,7 +570,7 @@ client.on('message', message =>
 				}
 				return;
 			}
-				post("Usage: /q <a> <b> <c>");
+				ch.send({files: ['https://cdn.discordapp.com/attachments/563998568940306437/573785938443108352/unknown.png']} + "Find roots, factors for quadratic equations using Burgerbotz quadratic equation solver!\nUsage: /q <a> <b> <c>");
 				break;
 					
 			case "coinflip":
