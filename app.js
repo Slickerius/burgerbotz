4de9d476084adc4ddd2a439b090d76b5ea30e7f9
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2019-07-21 12:34:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -765,17 +765,15 @@ client.on('message', message =>
 					var high = content['Time Series (Daily)'][date]['2. high'];
 					var low = content['Time Series (Daily)'][date]['3. low'];
 					
-					var change = ((parseInt(content['Time Series (Daily)'][prevDate]['4. close']) - parseInt(content['Time Series (Daily)'][date]['4. close'])) / parseInt(content['Time Series (Daily)'][prevDate]['4. close'])) * 100;
+					var change = ((parseFloat(content['Time Series (Daily)'][prevDate]['4. close']) - parseFloat(content['Time Series (Daily)'][date]['4. close'])) / parseFloat(content['Time Series (Daily)'][prevDate]['4. close'])) * 100;
 					change = change.toFixed(2);
 					change = Math.abs(change);
 					
-					console.log(change + " " + parseInt(content['Time Series (Daily)'][prevDate]['4. close']) + " " + parseInt(content['Time Series (Daily)'][date]['4. close']));
-					
-					if(parseInt(content['Time Series (Daily)'][date]['4. close']) > parseInt(content['Time Series (Daily)'][prevDate]['4. close']))
+					if(parseFloat(content['Time Series (Daily)'][date]['4. close']) > parseFloat(content['Time Series (Daily)'][prevDate]['4. close']))
 					{
-						post("__**" + args[1].toUpperCase() + "**__: **" + close + "** :small_red_triangle:*" + change + "%*\nOpen: **" + open + "**\nDay High: **" + high + "**\nDay Low: **" + low + "**\nPrevious Close: **" + prevClose + "**");
+						post("__**" + args[1].toUpperCase() + "**__: **" + close + "** :small_red_triangle:" + change + "%\nOpen: **" + open + "**\nDay High: **" + high + "**\nDay Low: **" + low + "**\nPrevious Close: **" + prevClose + "**");
 					} else {
-						post("__**" + args[1].toUpperCase() + "**__: **" + close + "** :small_red_triangle_down:*" + change + "%*\nOpen: **" + open + "**\nDay High: **" + high + "**\nDay Low: **" + low + "**\nPrevious Close: **" + prevClose + "**");
+						post("__**" + args[1].toUpperCase() + "**__: **" + close + "** :small_red_triangle_down:-" + change + "%\nOpen: **" + open + "**\nDay High: **" + high + "**\nDay Low: **" + low + "**\nPrevious Close: **" + prevClose + "**");
 					}
 				});
 				break;
