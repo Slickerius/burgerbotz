e7c148a08f962e795e332af6aeb3d1409fd4755f
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2019-07-29 21:33:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -813,7 +813,7 @@ client.on('message', message =>
 					} else if(prevDate.getMonth() >= 9) {
 						prevDate = prevDate.getFullYear() + '-' + (prevDate.getMonth() + 1) + '-' + (prevDate.getDate() - 1);
 					}
-					
+					console.log(date);	
 					var close = content['Time Series (Daily)'][date]['4. close'];
 					var prevClose = content['Time Series (Daily)'][prevDate]['4. close'];
 					var open = content['Time Series (Daily)'][date]['1. open'];
