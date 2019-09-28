1f8487b3c6dd931299a631bd055f854604961d24
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2019-09-28 21:36:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1100,7 +1100,7 @@ client.on('message', message =>
 					{
 						x += 1;
 						var req = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + i + "&apikey=" + stockApiKey;
-						k += "**[" + x + "] __" + i + "__**\nAmount : **" + db[sender.id]['stocks'][i] + ":scroll:**";
+						k += "\n**[" + x + "] __" + i + "__**\nAmount : **" + db[sender.id]['stocks'][i] + ":scroll:**";
 					}
 					k += "*For more information regarding a specific stock in your portfolio, kindly type /check <stock>.*";
 					post(k);
