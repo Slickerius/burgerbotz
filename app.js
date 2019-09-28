953898799d6a82c6529aea44ccd6945ded145d16
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2019-09-28 23:54:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1104,7 +1104,7 @@ client.on('message', message =>
 					db = JSON.parse(body);
 					if(db[sender.id] == null) db[sender.id] = {burgers: 100};
 					if(isNaN(db[sender.id].burgers)) db[sender.id].burgers = 100;
-					if(db[sender.id]['stock'] == null) 
+					if(db[sender.id]['stocks'] == null) 
 					{
 						post(":octagonal_sign: **Your stock portfolio is empty!**");
 						return;
