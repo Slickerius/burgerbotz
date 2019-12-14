7dfe03b636fe2ca2f49637444b39b241888fa25e
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2019-12-14 12:42:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1402,7 +1402,7 @@ client.on('message', message =>
 									if(isNaN(db[x].burgers)) db[x].burgers = 100;
 									db[x].burgers += parseFloat(arg0);							
 									db[sender.id].burgers -= parseFloat(arg0);
-									db[gdp] = parseFloat(arg0);
+									if(!db[gdp]) db[gdp] = parseFloat(arg0);
 									request(
 									{
   										method: "PUT",
