dbcb8d5a89cfa7884ca82f4999f6fc61740f0f21
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2019-12-16 14:41:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -330,10 +330,10 @@ client.on('message', message =>
 						if(message.content.startsWith("1"))
 						{
 							stage = randomize(1, 4);
-							randomEvents.call(ch, sender, 1, stage, value);	
+							randomEvents.call(ch, sender, 2, stage, value);	
 						} else if(message.content.startsWith("2")) {
 							stage = 4;
-							randomEvents.call(ch, sender, 1, 4, value);	
+							randomEvents.call(ch, sender, 2, 4, value);	
 						} else {
 							return;	
 						}
