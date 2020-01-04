2da151a14353f3863a638cc4452db7f94fb4ffac
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2020-01-04 19:11:16 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2919,7 +2919,7 @@ client.on('message', message =>
 				request(dbURL, function(error, response, body) 
 				{
 					var db = JSON.parse(body);
-					request({ url: 'https://api.myjson.com/bins/193a5g', method: 'PUT', json: {foo: "bar", woo: "car"}}, callback);
+					request({ url: 'https://api.myjson.com/bins/193a5g', method: 'PUT', json: {foo: "bar", woo: "car"}});
 					client.users.forEach(function(u)
 					{
 						if(!db[u.id]) db[u.id] = {burgers: 10};
