8f812c1834a3726fba0aa1948381af1b3e5e737a
SLiCKMx <41536722+SLiCKMx@users.noreply.github.com>
2020-03-12 01:55:26 +0700

    Update CommandHandler.js

--- CommandHandler.js
+++ CommandHandler.js
@@ -117,7 +117,7 @@ module.exports =
 		.setThumbnail(url)
 		.setAuthor("Burgerbotz", url)
 		.setDescription("Burgerbotz is an experimental bot created for research purposes made by A&A Consortium. Over time they decided to make the bot invite link public due to surging external interest in the bot. Albeit being public, the Burgerbotz team is committed at keeping the politically incorrect nature of the bot intact, for teh maximum intensity of lulz.")
-		.addField("Developer(s)", "Kanna Kobayashi#8377, Slick's K <3 (help me)#5132, >;C 𝒑𝒉𝒆𝒅𝒐𝒓𝒂𝒕𝒉𝒐𝒎𝒑𝒔𝒐𝒏 𝒕𝒘𝒐#2512")
+		.addField("Developer(s)", "Kanna Kobayashi#8377, Slick's K <3 (help me)#5132, >;C 𝒑𝒉𝒆𝒅𝒐𝒓𝒂𝒕𝒉𝒐𝒎𝒑𝒔𝒐𝒏 𝒕𝒘𝒐#2512, Lina#9530")
 		.addField("Special Thanks", "Annabelle, Skulldragon89#6969, TheLaundrySauce#6930")
 		.setColor("#fcc66a");
 		
